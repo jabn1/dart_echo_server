ea095162745269dc6748540465a5f2d8dacebd97
Jose <jose.berges23@gmail.com>
2021-02-20 21:49:46 -0400

    Add echo_client

--- echo_client/bin/echo_client.dart
+++ echo_client/bin/echo_client.dart
@@ -1,3 +1,12 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+import 'dart:io';
+import 'dart:typed_data';
+
+void main() async {
+  final client = await Socket.connect('localhost', 5000);
+
+  client.listen((Uint8List data) {
+    print(String.fromCharCodes(data));
+    client.write(stdin.readLineSync());
+  });
+  client.write(stdin.readLineSync());
 }
