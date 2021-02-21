8fe80313b49765c14a5b91dbf1c67daec56e31f6
Jose <jose.berges23@gmail.com>
2021-02-20 21:46:19 -0400

    Add echo_server

--- echo_server/bin/echo_server.dart
+++ echo_server/bin/echo_server.dart
@@ -1,3 +1,11 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+import 'dart:io';
+import 'dart:typed_data';
+
+void main() async {
+  final server = await ServerSocket.bind('localhost', 5000);
+  server.listen((client){
+    client.listen((Uint8List data) {
+      client.write(String.fromCharCodes(data));
+    });
+  });
 }
